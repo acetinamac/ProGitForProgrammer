2cf0ebdd74b4f2d03375b020ac0ec21ba37f1217
LAPTOP-AAUPJDC7\Armin Cetina Mac <ext.armin.cetina@coloit.com>
2023-03-14 19:38:28 -0500

    Add writeline indicating we are in command line

--- ProGitForProgrammer/Program.cs
+++ ProGitForProgrammer/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this in visual Studio");
+Console.WriteLine("I Just added this in the command line repo");
+
